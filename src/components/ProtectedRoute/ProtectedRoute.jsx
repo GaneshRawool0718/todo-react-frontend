8755938fa60a2fde9aa63ba55d754c9ebdccb4a2
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2025-07-15 14:42:54 +0530

    feat(route): add ProtectedRoute for Check if the user is authenticated and has the required role

---
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+// src/components/ProtectedRoute/ProtectedRoute.js
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children, allowedRoles }) => {
+  const token = localStorage.getItem('token');
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (!token || !user || !allowedRoles.includes(user.role)) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
